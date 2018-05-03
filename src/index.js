f02d9d8942fa2e772f56ed627d1ab4737ac3ba41
Aleksandr <alexdplua@gmail.com>
2018-05-03 21:32:52 +0300

    1 page, 2 page, 3 page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tabs').tabs();
 
-
+    $('.product-set__item').click(function () {
+        $(this)
+            .addClass('active')
+            .parent()
+            .children('span')
+            .not(this)
+            .removeClass('active')
+        
+    })
 
     function setHeight() {
         $('.product__img').height($('.product__img').width());
